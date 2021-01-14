0c3e5f85b2149a7d7326d281ac910d112ee1307e
Bruna Fonseca <bk.fonseca29@gmail.com>
2021-01-14 18:23:24 +0100

    Sudoku

---
+++ AppSudoku/FrmPrincipal.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace AppSudoku
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuPrincipal = new System.Windows.Forms.MenuStrip();
+            this.mnuFichier = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFichierNouvellePartie = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFichierSep1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuFichierQuitter = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAide = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuAideSolutionner = new System.Windows.Forms.ToolStripMenuItem();
+            this.panSeparateurVertical1 = new System.Windows.Forms.Panel();
+            this.panSeparateurVertical2 = new System.Windows.Forms.Panel();
+            this.panSeparateurHorizontal1 = new System.Windows.Forms.Panel();
+            this.panSeparateurHorizontal2 = new System.Windows.Forms.Panel();
+            this.panJeu = new System.Windows.Forms.Panel();
+            this.viaGrille = new VisualArrays.VisualIntArray();
+            this.menuPrincipal.SuspendLayout();
+            this.panJeu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuPrincipal
+            // 
+            this.menuPrincipal.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuPrincipal.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFichier,
+            this.mnuAide});
+            this.menuPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.menuPrincipal.Name = "menuPrincipal";
+            this.menuPrincipal.Size = new System.Drawing.Size(693, 35);
+            this.menuPrincipal.TabIndex = 6;
+            this.menuPrincipal.Text = "menuStrip1";
+            // 
+            // mnuFichier
+            // 
+            this.mnuFichier.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFichierNouvellePartie,
+            this.mnuFichierSep1,
+            this.mnuFichierQuitter});
+            this.mnuFichier.Name = "mnuFichier";
+            this.mnuFichier.Size = new System.Drawing.Size(78, 29);
+            this.mnuFichier.Text = "&Fichier";
+            // 
+            // mnuFichierNouvellePartie
+            // 
+            this.mnuFichierNouvellePartie.Name = "mnuFichierNouvellePartie";
+            this.mnuFichierNouvellePartie.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.mnuFichierNouvellePartie.Size = new System.Drawing.Size(297, 34);
+            this.mnuFichierNouvellePartie.Text = "&Nouvelle partie";
+            this.mnuFichierNouvellePartie.Click += new System.EventHandler(this.mnuFichierNouvellePartie_Click);
+            // 
+            // mnuFichierSep1
+            // 
+            this.mnuFichierSep1.Name = "mnuFichierSep1";
+            this.mnuFichierSep1.Size = new System.Drawing.Size(294, 6);
+            // 
+            // mnuFichierQuitter
+            // 
+            this.mnuFichierQuitter.Name = "mnuFichierQuitter";
+            this.mnuFichierQuitter.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.mnuFichierQuitter.Size = new System.Drawing.Size(297, 34);
+            this.mnuFichierQuitter.Text = "&Quitter";
+            this.mnuFichierQuitter.Click += new System.EventHandler(this.mnuFichierQuitter_Click);
+            // 
+            // mnuAide
+            // 
+            this.mnuAide.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuAideSolutionner});
+            this.mnuAide.Name = "mnuAide";
+            this.mnuAide.Size = new System.Drawing.Size(64, 29);
+            this.mnuAide.Text = "&Aide";
+            // 
+            // mnuAideSolutionner
+            // 
+            this.mnuAideSolutionner.Name = "mnuAideSolutionner";
+            this.mnuAideSolutionner.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.mnuAideSolutionner.Size = new System.Drawing.Size(266, 34);
+            this.mnuAideSolutionner.Text = "&Solutionner";
+            this.mnuAideSolutionner.Click += new System.EventHandler(this.mnuAideSolutionner_Click);
+            // 
+            // panSeparateurVertical1
+            // 
+            this.panSeparateurVertical1.BackColor = System.Drawing.Color.Gray;
+            this.panSeparateurVertical1.Location = new System.Drawing.Point(219, 20);
+            this.panSeparateurVertical1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panSeparateurVertical1.Name = "panSeparateurVertical1";
+            this.panSeparateurVertical1.Size = new System.Drawing.Size(8, 569);
+            this.panSeparateurVertical1.TabIndex = 7;
+            // 
+            // panSeparateurVertical2
+            // 
+            this.panSeparateurVertical2.BackColor = System.Drawing.Color.Gray;
+            this.panSeparateurVertical2.Location = new System.Drawing.Point(418, 20);
+            this.panSeparateurVertical2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panSeparateurVertical2.Name = "panSeparateurVertical2";
+            this.panSeparateurVertical2.Size = new System.Drawing.Size(8, 569);
+            this.panSeparateurVertical2.TabIndex = 8;
+            // 
+            // panSeparateurHorizontal1
+            // 
+            this.panSeparateurHorizontal1.BackColor = System.Drawing.Color.Gray;
+            this.panSeparateurHorizontal1.Location = new System.Drawing.Point(20, 206);
+            this.panSeparateurHorizontal1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panSeparateurHorizontal1.Name = "panSeparateurHorizontal1";
+            this.panSeparateurHorizontal1.Size = new System.Drawing.Size(609, 8);
+            this.panSeparateurHorizontal1.TabIndex = 8;
+            // 
+            // panSeparateurHorizontal2
+            // 
+            this.panSeparateurHorizontal2.BackColor = System.Drawing.Color.Gray;
+            this.panSeparateurHorizontal2.Location = new System.Drawing.Point(20, 395);
+            this.panSeparateurHorizontal2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panSeparateurHorizontal2.Name = "panSeparateurHorizontal2";
+            this.panSeparateurHorizontal2.Size = new System.Drawing.Size(609, 8);
+            this.panSeparateurHorizontal2.TabIndex = 9;
+            // 
+            // panJeu
+            // 
+            this.panJeu.BackColor = System.Drawing.Color.Green;
+            this.panJeu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panJeu.Controls.Add(this.panSeparateurHorizontal2);
+            this.panJeu.Controls.Add(this.panSeparateurVertical2);
+            this.panJeu.Controls.Add(this.panSeparateurHorizontal1);
+            this.panJeu.Controls.Add(this.panSeparateurVertical1);
+            this.panJeu.Controls.Add(this.viaGrille);
+            this.panJeu.Location = new System.Drawing.Point(22, 62);
+            this.panJeu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panJeu.Name = "panJeu";
+            this.panJeu.Size = new System.Drawing.Size(648, 611);
+            this.panJeu.TabIndex = 11;
+            // 
+            // viaGrille
+            // 
+            this.viaGrille.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.viaGrille.CellMargin = 4;
+            this.viaGrille.CellSize = new System.Drawing.Size(59, 54);
+            this.viaGrille.ColumnCount = 9;
+            this.viaGrille.DisabledAppearance.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.viaGrille.DisabledAppearance.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viaGrille.DisabledAppearance.Shape = VisualArrays.enuShape.RoundRect;
+            this.viaGrille.DisabledAppearance.Style = VisualArrays.enuBkgStyle.FillShape;
+            this.viaGrille.DisabledAppearance.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.viaGrille.EnabledAppearance.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.viaGrille.EnabledAppearance.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viaGrille.EnabledAppearance.Shape = VisualArrays.enuShape.RoundRect;
+            this.viaGrille.EnabledAppearance.TextColor = System.Drawing.Color.Black;
+            this.viaGrille.GridAppearance.Color = System.Drawing.Color.Silver;
+            this.viaGrille.Location = new System.Drawing.Point(9, 11);
+            this.viaGrille.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.viaGrille.Maximum = 9;
+            this.viaGrille.Minimum = 0;
+            this.viaGrille.Name = "viaGrille";
+            this.viaGrille.RowCount = 9;
+            this.viaGrille.RowHeader.ForeColor = System.Drawing.Color.White;
+            this.viaGrille.SelectionAppearance.Padding = new System.Windows.Forms.Padding(2, 2, 1, 1);
+            this.viaGrille.SelectionAppearance.Shape = VisualArrays.enuShape.RoundRect;
+            this.viaGrille.SelectionMode = System.Windows.Forms.SelectionMode.One;
+            this.viaGrille.Size = new System.Drawing.Size(625, 580);
+            this.viaGrille.SpecialValue = 0;
+            this.viaGrille.TabIndex = 12;
+            this.viaGrille.ValueChanged += new System.EventHandler<VisualArrays.ValueChangedEventArgs>(this.viaGrille_ValueChanged);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 697);
+            this.Controls.Add(this.menuPrincipal);
+            this.Controls.Add(this.panJeu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jeu du Sudoku ";
+  
+            this.menuPrincipal.ResumeLayout(false);
+            this.menuPrincipal.PerformLayout();
+            this.panJeu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem mnuFichier;
+        private System.Windows.Forms.ToolStripMenuItem mnuFichierQuitter;
+        private System.Windows.Forms.Panel panSeparateurVertical1;
+        private System.Windows.Forms.Panel panSeparateurVertical2;
+        private System.Windows.Forms.Panel panSeparateurHorizontal1;
+        private System.Windows.Forms.Panel panSeparateurHorizontal2;
+        private System.Windows.Forms.ToolStripMenuItem mnuFichierNouvellePartie;
+        private System.Windows.Forms.ToolStripSeparator mnuFichierSep1;
+        private System.Windows.Forms.Panel panJeu;
+        private VisualArrays.VisualIntArray viaGrille;
+        private System.Windows.Forms.ToolStripMenuItem mnuAide;
+        private System.Windows.Forms.ToolStripMenuItem mnuAideSolutionner;
+    }
+}
+
---
+++ AppSudoku/FrmPrincipal.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using VisualArrays;
+
+namespace AppSudoku
+{
+    public partial class FrmPrincipal : Form
+    {
+        /// <summary>
+        /// NOTIONS :
+        ///  - Copier des éléments d'un tableau 2D dans une grille
+        ///  - Désactiver des cellules de la grille
+        ///  - Utiliser une valeur spéciale dont l'apparence est différente des autres valeurs
+        ///  - Parcourir les cellules d'une grille en deux dimensions
+        ///     - Parcourir des rangées, des colonnes ou des zones de 3 X 3
+        ///  - Utiliser un tableau de booléens pour marquer les nombres utilisés.
+        /// </summary>
+
+        public const string APP_INFO = "(Matériel)";
+
+        #region Sudoku Moyen (SEUL LE MOYEN EST UTILISÉ)
+        int[,] m_tabSudokuMoyen =
+        {
+            {-5,2,4,3,1,7,-8,6,-9},
+            {3,1,7,-6,-8,-9,4,5,2},
+            {-8,-9,6,4,-2,5,1,-3,7},
+            {2,8,1,-9,3,-6,5,-7,4},
+            {-4,6,-5,8,-7,2,-3,9,-1},
+            {9,-7,3,-5,4,-1,6,2,8},
+            {7,-5,8,1,-9,3,2,-4,-6},
+            {6,4,9,-2,-5,-8,7,1,3},
+            {-1,3,-2,7,6,4,9,8,-5}
+        };
+        #region Sudoku Facile
+        int[,] m_tabSudokuFacile = {
+            {5,1,-2,-4,8,6,9,-7,-3},
+            {6,-7,-9,2,3,-1,-5,-4,8},
+            {-8,4,3,-5,9,-7,2,6,-1},
+            {2,3,5,7,1,8,4,9,-6},
+            {7,-9,1,-3,-6,-4,8,-2,5},
+            {-4,6,8,9,5,2,3,1,7},
+            {-1,5,7,-8,2,-9,6,3,-4},
+            {3,-2,-6,-1,4,5,-7,8,9},
+            {-9,-8,4,6,7,-3,-1,5,2},
+        };
+        #endregion
+        #region Sudoku Difficile
+        int[,] m_tabSudokuDifficile =
+        {
+            {5,9,6,-2,7,-4,-8,1,3},
+            {-7,2,8,1,3,9,6,-4,-5},
+            {4,-3,1,5,8,-6,-2,7,-9},
+            {-9,4,3,-6,-1,2,-5,8,7},
+            {-8,5,2,3,9,7,1,6,-4},
+            {6,1,-7,4,-5,-8,9,3,-2},
+            {-3,8,-4,-9,6,5,7,-2,1},
+            {-1,-7,5,8,2,3,4,9,-6},
+            {2,6,-9,-7,4,-1,3,5,8 }
+        };
+        #endregion
+        #endregion
+
+        #region NE PAS MODIFIER
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            Text += APP_INFO;
+            PréparerNouvellePartie();
+        }
+        /// <summary>
+        /// Démarre une nouvelle partie
+        /// </summary>
+        private void mnuFichierNouvellePartie_Click(object sender, EventArgs e)
+        {
+            PréparerNouvellePartie();
+        }
+        //================================================================================================
+        private void mnuFichierQuitter_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        #endregion
+
+        #region PréparerNouvellePartie
+
+        private void PréparerNouvellePartie()
+        {
+            
+            mnuAideSolutionner.Enabled = true;
+            viaGrille.Enabled = true;
+            viaGrille.Clear();
+
+            for (int rangée = 0; rangée < viaGrille.RowCount; rangée++)
+            {
+                for(int colonne = 0; colonne < viaGrille.ColumnCount; colonne++)
+                {
+                    if (m_tabSudokuMoyen[rangée,colonne] < 0)
+                    {
+                        viaGrille[rangée, colonne] = -m_tabSudokuMoyen[rangée, colonne];
+                        
+                        viaGrille.DisableCell(rangée,colonne);
+                    }
+                }
+            }
+            
+        }
+        #endregion
+
+        #region Solutionner le Sudoku
+
+        private void mnuAideSolutionner_Click(object sender, EventArgs e)
+        {
+            
+
+            for (int rangée = 0; rangée < viaGrille.RowCount; rangée++)
+            {
+                for (int colonne = 0; colonne < viaGrille.ColumnCount; colonne++)
+                {
+                    if (m_tabSudokuMoyen[rangée, colonne] > 0)
+                    {
+                        viaGrille[rangée, colonne] = m_tabSudokuMoyen[rangée, colonne];
+                        viaGrille.SelectedIndex = -1;
+                        
+                    }
+                }
+                
+            }
+            mnuAideSolutionner.Enabled = false;
+            panJeu.BackColor = Color.Orange;
+            viaGrille.Enabled = false;
+            
+        }
+        #endregion
+
+        #region VérifierLaFinDeLaPartie
+
+        private void VérifierLaFinDeLaPartie()
+        {
+            int cptZéro = 0;
+            for (int rangée = 0; rangée < viaGrille.RowCount; rangée++)
+            {
+                for (int colonne = 0; colonne < viaGrille.ColumnCount; colonne++)
+                {
+                    if(viaGrille[rangée,colonne] == 0)
+                    {
+                        cptZéro++;
+                    }
+                }
+
+            }
+            if (cptZéro == 0 && panJeu.BackColor == Color.Green)
+            {
+                panJeu.BackColor = Color.Orange;
+                viaGrille.Enabled = false;
+            }
+
+        }
+        #endregion
+
+        #region Vérifier l'intégrité du Sudoku
+
+        private void VérifierToutesLesRangées()
+        {
+            
+
+            for (int rangée = 0; rangée < viaGrille.RowCount; rangée++)
+            {
+                bool[] tabDesChiffresPrésents = { false, false, false, false, false, false, false, false, false, false };
+         
+                for (int colonne = 0; colonne < viaGrille.ColumnCount; colonne++)
+                {
+                    int chiffreCourant = viaGrille[rangée,colonne];
+                    if (chiffreCourant != 0)
+                    {
+                        
+                        if (tabDesChiffresPrésents[chiffreCourant] == false)
+                        {
+                            tabDesChiffresPrésents[chiffreCourant] = true; 
+                        }
+                        else
+                        {
+                            panJeu.BackColor = Color.Red;
+                        }
+                        
+                    }
+                    
+                }
+            }
+        }
+
+        private void VérifierToutesLesColonnes()
+        {
+
+            for (int colonne = 0; colonne < viaGrille.ColumnCount; colonne++)
+            {
+
+                bool[] tabDesChiffresPrésents = { false, false, false, false, false, false, false, false, false, false };
+
+                for (int rangée = 0; rangée < viaGrille.RowCount; rangée++)
+                { 
+                    
+                    int chiffreCourant = viaGrille[rangée, colonne];
+                    if (chiffreCourant != 0)
+                    {
+                        if (tabDesChiffresPrésents[chiffreCourant] == false)
+                        {
+                            tabDesChiffresPrésents[chiffreCourant] = true;
+                        }
+                        else
+                        {
+                            panJeu.BackColor = Color.Red;
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+        private void VérifierToutesLesRegions()
+        {
+			// parcourir les régions 
+			for (int rangeeR = 0; rangeeR < 9; rangeeR+=3)
+            {
+                for (int colonneR = 0; colonneR < 9; colonneR+=3)
+                {
+					//  initialiser le tableau des chiffres à false
+                    bool[] tabDesChiffresPrésents = { false, false, false, false, false, false, false, false, false, false };
+					
+					//  parcourir l'intérieur de chaque des régions
+                    for (int rangee = rangeeR; rangee < rangeeR + 3; rangee++)
+					{	
+                        for (int colonne = colonneR; colonne < colonneR + 3; colonne++)
+                        {
+							//  prendre le chiffre courant dans la grille
+                            int chiffreCourant = viaGrille[rangee, colonne];
+                            //  vérifier l'intégrité
+                            if (chiffreCourant != 0)
+                            {   
+                                if (tabDesChiffresPrésents[chiffreCourant] == false)
+                                {
+                                    tabDesChiffresPrésents[chiffreCourant] = true;
+                                }
+                                else
+                                {
+                                    panJeu.BackColor = Color.Red;
+                                }
+                            }
+                        }
+					}
+                }
+            }
+
+        }
+        #endregion
+
+        #region viaGrille_ValueChanged (À CONSULTER/NE PAS MODIFIER)
+        //================================================================================================
+        /// <summary>
+        /// Vérifie l'intégrité du jeu et si la partie est terminée à chaque changement de valeur dans la grille.
+        /// </summary>
+        private void viaGrille_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            panJeu.BackColor = Color.Green;
+            VérifierToutesLesColonnes();
+            VérifierToutesLesRangées();
+            VérifierToutesLesRegions();
+            VérifierLaFinDeLaPartie();
+        }
+        #endregion
+
+      
+    }
+}
